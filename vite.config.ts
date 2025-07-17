985a200e9d4aec0f957d097264f5e0a51815fa44
sagarmehta <sagarmeht4@gmail.com>
2025-07-17 20:13:37 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,27 @@ import path from "path";
 import { componentTagger } from "lovable-tagger"; 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '',
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-  ],
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  }
+}));
