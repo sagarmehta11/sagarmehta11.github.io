4fecb42dd46c19cb66d0be1f0afe6ee00412efa0
sagarmehta <sagarmeht4@gmail.com>
2025-07-18 20:10:36 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/sagarmehta11.github.io',
   server: {
     host: "::",
     port: 8080,
